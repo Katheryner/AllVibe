e127ac88f51d8304a3b7b977c1ef7fc95e3c1338
JeisonOJ <jeisonortiz1516@gmail.com>
2024-05-03 12:21:06 -0500

    update entities

--- src/main/java/com/allvibe/all_vibe/domain/entities/Event.java
+++ src/main/java/com/allvibe/all_vibe/domain/entities/Event.java
@@ -1,33 +1,41 @@
 package com.allvibe.all_vibe.domain.entities;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import com.allvibe.all_vibe.util.enums.Status;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Entity(name = "festival")
+@Entity(name = "events")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Festival {
+public class Event {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private Status status;
     private LocalDate date;
     private int capacity;
     private String place;
-    private List<User> users;
-    private List<Artist> artists;
-    private Artist mainArtist;
     private String description;
+    private String eventType;
+    @OneToMany(mappedBy = "event", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private EventParticipation eventParticipation;
+
 }
--- src/main/java/com/allvibe/all_vibe/domain/entities/User.java
+++ src/main/java/com/allvibe/all_vibe/domain/entities/User.java
@@ -1,18 +1,39 @@
 package com.allvibe.all_vibe.domain.entities;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Entity(name = "user")
+@Entity(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
-    private Spectator spectator;
-    private Lecturer lecturer;
-    private Artist artist;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 40, nullable = false)
+    private String username;
+    @Column(length = 40, nullable = false)
+    private String email;
+    @Column(length = 40, nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private boolean isAdmin;
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private EventParticipation eventParticipation;
 
 }
