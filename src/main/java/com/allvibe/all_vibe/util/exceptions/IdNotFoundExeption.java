f35f4e9bf8aa7433485ef44d1d5bc848e27d372b
KeiberLLB <leandro1991.9@gmail.com>
2024-05-06 13:29:18 -0500

    Control Errors advance

---
+++ src/main/java/com/allvibe/all_vibe/util/exceptions/IdNotFoundExeption.java
@@ -0,0 +1,9 @@
+package com.allvibe.all_vibe.util.exceptions;
+
+public class IdNotFoundExeption extends RuntimeException {
+  private static final String ERROR_MESSAGE = "No hay registros en la entidad % con el id suministrado.";
+
+  public IdNotFoundExeption(String nameEntity) {
+    super(String.format(ERROR_MESSAGE, nameEntity));
+  }
+}
